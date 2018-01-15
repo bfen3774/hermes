74f8c69a1b53f9baa42eeb4e6b9a7c7db832b97b
Brandon Fenstermaker <brandonf@cdomvm.local>
2018-01-15 13:17:21 -0700

    working on models

--- hermes/urls.py
+++ hermes/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from lightning.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('lightning/', include('lightning.urls'))
+    path('lightning/', include('lightning.urls')),
+    path('', index, name='index')
 ]
--- lightning/models.py
+++ lightning/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Plan(models.Model):
+    name = models.CharField(max_length=300)
+    reddit_link = models.URLField(max_length=200)
+    user = models.CharField(max_length=200)
+    user_age = models.CharField(max_length=100)
+    user_starting_weight = models.IntegerField()
+    user_ending_weight = models.IntegerField()
+
