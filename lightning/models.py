7099b67ef97a1c133e7bd8d71d9a821f763b01a0
Brandon Fenstermaker <brandonf@cdomvm.local>
2018-01-16 12:44:21 -0700

    removed diet

--- lightning/models.py
+++ lightning/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Plan(models.Model):
     name = models.CharField(max_length=300)
-    reddit_link = models.URLField(max_length=200)
+    reddit_link = models.URLField(max_length=200, unique=True)
     user = models.CharField(max_length=200)
     user_age = models.CharField(max_length=100)
     user_sex = models.CharField(max_length=20)
@@ -14,15 +14,11 @@ class Plan(models.Model):
     user_starting_weight = models.IntegerField()
     user_ending_weight = models.IntegerField()
     goals = models.CharField(max_length=100)
-
-class Diet(models.Model):
-    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
     calories = models.IntegerField()
-    name = models.CharField(max_length=100)
     macro_protein = models.IntegerField()
     macro_carb = models.IntegerField()
     macro_fat = models.IntegerField()
 
 class Food(models.Model):
-    diet = models.ForeignKey(Diet, on_delete=models.CASCADE)
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
