f26f1e0cd9b655ebde11e76b3bcedcf74d1127b8
Brandon Fenstermaker <brandonf@cdomvm.local>
2018-01-16 14:00:57 -0700

    plan list

--- hermes/urls.py
+++ hermes/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from lightning.views import index
+from lightning.views import index, PlanListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
