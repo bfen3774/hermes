bf565e80a0f813259e0279cdf3f38eb47dd207d9
Brandon Fenstermaker <brandonf@cdomvm.local>
2018-01-18 14:49:46 -0700

    working on ajax

--- lightning/views.py
+++ lightning/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Plan
+from django.http import JsonResponse
 from django.utils import timezone
 
 # Create your views here.
@@ -14,3 +15,11 @@ class PlanListView(ListView):
         context = super().get_context_data(**kwargs)
         context['now'] = timezone.now()
         return context
+
+    def get_queryset(self):
+        minPeriod = self.request.GET.get('minPeriod')
+        maxPeriod = self.request.GET.get('maxPeriod')
+        if minPeriod:
+            return Plan.objects.filter(period__gte=minPeriod).filter(period__lt=maxPeriod)
+        else:
+            return Plan.objects.all()
